be793846bc03e666480bf8d7cd668a3fde33d9bc
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-05-20 16:14:43 +0700

    Update product details page

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,31 @@
 let controller = {};
 const models = require("../models");
 
+controller.getData = async (req, res, next) => {
+  const categories = await models.Category.findAll({
+    include: [
+      {
+        model: models.Product,
+      },
+    ],
+  });
+  res.locals.categories = categories;
+
+  const brands = await models.Brand.findAll({
+    include: [
+      {
+        model: models.Product,
+      },
+    ],
+  });
+  res.locals.brands = brands;
+
+  const tags = await models.Tag.findAll();
+  res.locals.tags = tags;
+
+  next();
+};
+
 controller.show = async (req, res) => {
   const category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
   const brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
@@ -49,6 +74,42 @@ controller.show = async (req, res) => {
   const products = await models.Product.findAll(options);
   res.locals.products = products;
   res.render("product-list");
+};
+
+controller.showDetails = async (req, res) => {
+  const id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
+  const product = await models.Product.findOne({
+    attributes: [
+      "id",
+      "name",
+      "stars",
+      "oldPrice",
+      "price",
+      "summary",
+      "description",
+      "specification",
+    ],
+    where: { id },
+    include: [
+      {
+        model: models.Image,
+        attributes: ["name", "imagePath"],
+      },
+      {
+        model: models.Review,
+        attributes: ["id", "review", "stars", "createdAt"],
+        include: [
+          {
+            model: models.User,
+            attributes: ["firstName", "lastName"],
+          },
+        ],
+      },
+    ],
+  });
+
+  res.locals.product = product;
+  res.render("product-detail");
 };
 
 module.exports = controller;
