71b1ef2790d488ca0a409928202989fef01e5aaf
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-05-25 18:56:47 +0700

    Using dotenv

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 "use strict";
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const expressHandlebars = require("express-handlebars");
 const { createStarList } = require("./controllers/handlebarsHelper");
 const { createPagination } = require("express-handlebars-paginate");
+// const redisStore = require("connect-redis").default;
+// const { createClient } = require("redis");
+// const redisClient = createClient({
+//   // url: "rediss://red-chnkcdhmbg5577mamjl0:JXBY7vmSdUmBHMyLhJDEwtyA1ta09h8V@oregon-redis.render.com:6379",
+//   url: "redis://red-chnkcdhmbg5577mamjl0:6379",
+// });
+
+// redisClient.connect().catch(console.error);
 
 // config public static folder
 app.use(express.static(__dirname + "/public"));
